a975bfa984ba2a599496718a5b35d1315b6b2dd7
Nack233 <65160233@go.buu.ac.th>
2024-01-29 17:27:51 +0700

    Update mypage.blade.php

--- resources/views/myfolder/mypage.blade.php
+++ resources/views/myfolder/mypage.blade.php
@@ -31,7 +31,6 @@
         font-family: Sriracha;
         }
         body{
-            /* background-image: url("https://i.pinimg.com/originals/8c/62/93/8c6293f2ccb3b36222301e5da06f6f60.gif"); */
             background-image: url("https://i.pinimg.com/736x/01/8a/ab/018aabb76e41b6b50c854c61005fbcff.jpg");
             background-size: cover;
         }
