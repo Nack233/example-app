1defc310873f4d3715f2ffd7a877f992161bec50
Nack233 <65160233@go.buu.ac.th>
2024-01-29 19:42:33 +0700

    gg

--- resources/views/titles/index.blade.php
+++ resources/views/titles/index.blade.php
@@ -93,11 +93,6 @@
                                         <td>
                                             <a href="{{ url('/titles/' . $title->tit_id) }}"
                                                 class="btn btn-warning">แก้ไข</a>
-                                            <form method="post" action="/titles/{{ $title->tit_id }}">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">ลบ</button>
-                                            </form>
                                         </td>
                                     </tr>
                                     <?php } ?>
