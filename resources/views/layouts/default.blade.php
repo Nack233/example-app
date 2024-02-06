f9c3da8b8e31feada53f3a2e4b6e3cd3895c4048
Nack233 <65160233@go.buu.ac.th>
2024-02-06 12:50:07 +0700

    logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -236,6 +236,12 @@
                                 </p>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a href="{{ route('logout') }}" class="nav-link">
+                                <i class="nav-icon fas fa-sign-out-alt"></i>
+                                <p>Logout</p>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
