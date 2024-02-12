bb7cdda184aa1f40b5cc45aa406a7942ee84406d
Nack233 <65160233@go.buu.ac.th>
2024-02-12 19:49:55 +0700

    save

--- app/Models/M_titles.php
+++ app/Models/M_titles.php
@@ -12,6 +12,5 @@ class M_titles extends Model
     protected $table = 'titles';
     protected $primaryKey = 'tit_id';
     public $incrementing = true;
-    public $LinesLamps = false;
-
+    public $timestamps = false;
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,109 +1,60 @@
-<!doctype html>
-<html>
+<?php
 
-<head>
-    <title>Javascript 101</title>
-</head>
+use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
+use App\Http\Controllers\MyController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-<body>
-    <h1>Before</h1>
-    <h1 id="myh1">
-        <h1>Prepend</h1>Javascript<h1>Append</h1>
-    </h1>
-    <h1>After</h1>
-    <button onclick="alert('Hello World!')">Click Me!</button>
-    <input type="text" id="my_number" value="10">
-    <button onclick="myFunction()">submit number</button>
-    <br>
-    <button onclick="myFunction2()">submit my function2</button>
-    <br>
-    <table id="my_table">
-        <thead>
-            <tr>
-                <td>#</td>
-                <td>result</td>
-            </tr>
-        </thead>
-        <tbody id="my_tbody">
 
-        </tbody>
-    </table>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            console.log("Hello World - document.ready")
-            console.log($('#myh1').text())
-            console.log($('#my_number').val())
-            $('#myh1').text("Javascript 101")
-            $('#my_number').val(100)
-            // setInterval(() => {
-            //     $('#myh1').after(`<h1 class="my_gen_number">setInterval</h1>`)
-            // }, 2000);
-        });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-        function myFunction2() {
-            // $($('#my_table').children()[1]).html("<tr><td>1</td><td>test</td></tr>")
-            let my_number = parseInt($('#my_number').val())
-            let my_code_tr = ``
-            for (let i = 0; i < my_number; i++) {
-                my_code_tr += `<tr><td>${i}</td><td>test</td></tr>`
-            }
-            $('#my_tbody').html(my_code_tr)
-        }
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
 
-        function myFunction() {
-            let my_number = parseInt($('#my_number').val())
-            for (let i = 0; i < my_number; i++) {
-                $('#myh1').after(`<h1 class="my_gen_number">${i}</h1>`)
-            }
-            console.log(document.getElementById('my_number').value);
-            console.log('Click submit number')
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware('auth')->group(function(){
+    // auth first
+});
+Route::get('/my-controller', [MyController::class, 'index']);
 
-            setTimeout(function() {
-                $('.my_gen_number').each(function(index, val) {
-                    $(val).remove();
-                    //val.remove()
-                })
-            }, 2000);
+Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
+Route::namespace('App\Http\Controllers')->group(function(){
+    Route::get('/my-controller3', 'MyController@index');
+    Route::post('/my-controller3-post', 'MyController@store');
+});
 
-        }
-        console.log("Hello World!")
-        let myval;
-        console.log(typeof myval);
-        myval = '10';
-        myval2 = '2';
-        console.log(myval, myval2);
-        myval3 = parseInt(myval) + myval2;
-        console.log(myval3)
-        myval3 = myval - myval2;
-        console.log(myval3)
-        myval3 = myval * myval2;
-        console.log(myval3)
-        myval3 = myval / myval2;
-        console.log(myval3)
-    </script>
-    <script>
-        let myval4 = [1, 2, 3, 4];
+Route::resource('/my-controller4', MyController::class);
 
-        myval4[5] = 5;
-        myval4[6] = [6, 5, 4, 5]
-        console.log(myval4)
 
-        for (i = 0; i < myval4.length; i++) {
-            console.log("in for", myval4[i])
-        }
-        myval4.forEach(function(value, index) {
-            console.log("in forEach", value, index)
-        });
+Route::get('/', function () {
+    return view('welcome'); // welcome.blade.php
+});
 
-        console.log(document.getElementById('myh1').innerHTML)
-        /*
-        // php
-        foreach($myval4 as $index => $value){
+// use Illuminate\Http\Request;
 
-        }
-        */
-    </script>
-</body>
+Route::get('/my-route', function(){
+    // return view('myroute');
+    //        Key    =>  Value
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Laravel";
+    return view('myfolder.mypage',$data);
+});
 
-</html>
+
+Route::post('/my-route', function(Request $req){
+    $data['myinput'] =  $req->input('myinput');
+    return view('myroute', $data);
+});
